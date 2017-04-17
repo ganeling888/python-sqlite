95799b389ada7baf43205fd243aa8d6088b2006f
Chris John Ryan <chris.john.ryan93@gmail.com>
2017-04-16 21:10:02 -0400

    Adding a few more comments.

--- get_files_in_directory.py
+++ get_files_in_directory.py
@@ -1,3 +1,5 @@
 import os
+# Go through every file in the directory that this script was executed in
 for filename in os.listdir(os.getcwd()):
+	# Print the filenames for each file
 	print(filename)
